767ee42be9812bd5432039c1bab067edd5e39d02
Radek Burget <burgetr@fit.vutbr.cz>
2014-11-16 23:37:58 +0100

    Grid display implementation

--- src/main/java/org/fit/segm/grouping/GridTopology.java
+++ src/main/java/org/fit/segm/grouping/GridTopology.java
@@ -5,6 +5,10 @@
  */
 package org.fit.segm.grouping;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+import org.fit.layout.api.OutputDisplay;
 import org.fit.layout.model.Area;
 import org.fit.layout.model.AreaTopology;
 import org.fit.layout.model.Rectangular;
@@ -22,6 +26,11 @@ public class GridTopology implements AreaTopology
         this.area = area;
     }
 
+    public Area getArea()
+    {
+        return area;
+    }
+    
     @Override
     public Rectangular getPosition()
     {
@@ -52,4 +61,31 @@ public class GridTopology implements AreaTopology
         return area.getGrid().getMaxIndent();
     }
 
+    @Override
+    public void drawLayout(OutputDisplay disp)
+    {
+        Graphics ig = disp.getGraphics();
+        AreaGrid grid = area.getGrid();
+        Color c = ig.getColor();
+        ig.setColor(Color.BLUE);
+        int xo = getArea().getX1();
+        for (int i = 1; i <= grid.getWidth(); i++)
+        {
+            xo += grid.getCols()[i-1];
+            /*System.out.println(i + " : " + xo);
+            if (i == 42) ig.setColor(Color.GREEN);
+            else if (i == 47) ig.setColor(Color.RED);
+            else ig.setColor(Color.BLUE);*/
+            ig.drawLine(xo, getArea().getY1(), xo, getArea().getY2());
+        }
+        int yo = getArea().getY1();
+        for (int i = 0; i < grid.getHeight(); i++)
+        {
+            yo += grid.getRows()[i];
+            ig.drawLine(getArea().getX1(), yo, getArea().getX2(), yo);
+        }
+        ig.setColor(c);
+        
+    }
+    
 }
