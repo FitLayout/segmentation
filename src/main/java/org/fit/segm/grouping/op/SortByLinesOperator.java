2fd481346536ea3b31167d29d982d4ad6a301ffc
Radek Burget <burgetr@fit.vutbr.cz>
2015-09-23 15:53:09 +0200

    Line detection simplification

--- src/main/java/org/fit/segm/grouping/op/SortByLinesOperator.java
+++ src/main/java/org/fit/segm/grouping/op/SortByLinesOperator.java
@@ -116,9 +116,15 @@ public class SortByLinesOperator extends SortByPositionOperator
         Vector<Area> ret = new Vector<Area>();
         ret.add(area);
         
+        final int nx1 = area.getGridPosition().getX1();
         final int ny1 = area.getGridPosition().getY1();
         final int nx2 = area.getGridPosition().getX2();
         final int ny2 = area.getGridPosition().getY2();
+        
+        //the maximal Y difference to consider other areas to be on the same line
+        int threshold = (area.getHeight() / 2) - 1;
+        if (threshold < 0) threshold = 0;
+        System.out.println("Thr:" + threshold);
         
         //try to expand to the right
         int dist = 1;
@@ -131,9 +137,29 @@ public class SortByLinesOperator extends SortByPositionOperator
                 if (neigh != null && candidates.contains(neigh)) //something found
                 {
                     //if (isOnSameLine(parent, area, neigh)) //check if the nodes could be joined
-                    if (AreaUtils.isOnSameLine(area, neigh, 2))
+                    if (AreaUtils.isOnSameLine(area, neigh, threshold))
                     {
                         ret.add(neigh);
+                        break;
+                    }
+                }
+            }
+            dist++;
+        }
+        //try to expand to the left
+        dist = 1;
+        while (nx1 - dist >= 0)
+        {
+            //try to find some node at the right in the given distance
+            for (int y = ny1; y <= ny2; y++)
+            {
+                AreaImpl neigh = (AreaImpl) parent.getGrid().getAreaAt(nx1 - dist, y);
+                if (neigh != null && candidates.contains(neigh)) //something found
+                {
+                    //if (isOnSameLine(parent, area, neigh)) //check if the nodes could be joined
+                    if (AreaUtils.isOnSameLine(area, neigh, threshold))
+                    {
+                        ret.insertElementAt(neigh, 0);
                         break;
                     }
                 }
@@ -144,81 +170,4 @@ public class SortByLinesOperator extends SortByPositionOperator
         return ret;
     }
     
-    /**
-     * Joins two boxes horizontally into one area if the node heights are equal or they 
-     * can be aligned to a rectangle using free spaces.
-     * @param n1 left node to be aligned
-     * @param n2 right node to be aligned
-     * @return <code>true</code> when succeeded
-     */
-    private boolean isOnSameLine(AreaImpl parent, AreaImpl n1, AreaImpl n2)
-    {
-        //System.out.println("HJoin: " + n1.toString() + " + " + n2.toString());
-        //align the start
-        int sy1 = n1.getGridPosition().getY1();
-        int sy2 = n2.getGridPosition().getY1();
-        while (sy1 != sy2)
-        {
-            if (sy1 < sy2) //n1 starts earlier, try to expand n2 up
-            {
-                if (sy2 > 0 && canExpandY(parent, n2, sy2-1, n1))
-                    sy2--;
-                else
-                    return false; //cannot align - give up
-            }
-            else if (sy1 > sy2) //n2 starts earlier, try to expand n1 up
-            {
-                if (sy1 > 0 && canExpandY(parent, n1, sy1-1, n2))
-                    sy1--;
-                else
-                    return false; //cannot align - give up
-            }
-        }
-        //System.out.println("sy1="+sy1);
-        //align the end
-        int ey1 = n1.getGridPosition().getY2(); //last
-        int ey2 = n2.getGridPosition().getY2();
-        while (ey1 != ey2)
-        {
-            if (ey1 < ey2) //n1 ends earlier, try to expand n1 down
-            {
-                if (ey1 < parent.getGrid().getWidth()-1 && canExpandY(parent, n1, ey1+1, n2))
-                    ey1++;
-                else
-                    return false; //cannot align - give up
-            }
-            else if (ey1 > ey2) //n2 ends earlier, try to expand n2 down
-            {
-                if (ey2 < parent.getGrid().getWidth()-1 && canExpandY(parent, n2, ey2+1, n1))
-                    ey2++;
-                else
-                    return false; //cannot align - give up
-            }
-        }
-        //System.out.println("ey1="+ey1);
-        //align succeeded
-        return true;
-    }
-    
-    
-    /**
-     * Checks if the area can be vertically expanded to the given 
-     * Y coordinate, i.e. there is a free space in the space on this Y coordinate
-     * for the whole width of the area.
-     * @param node the area node that should be expanded
-     * @param y the Y coordinate to that the area should be expanded
-     * @param except an area that shouldn't be considered for conflicts (e.g. an overlaping area)
-     * @return <code>true</code> if the area can be expanded
-     */
-    private boolean canExpandY(AreaImpl parent, AreaImpl node, int y, AreaImpl except)
-    {
-        for (int x = node.getGridX(); x < node.getGridX() + node.getGridWidth(); x++)
-        {
-            AreaImpl cand = (AreaImpl) parent.getGrid().getAreaAt(x, y);
-            if (cand != null && cand != except)
-                return false; //something found - cannot expand
-        }
-        return true;
-    }
-
 }
